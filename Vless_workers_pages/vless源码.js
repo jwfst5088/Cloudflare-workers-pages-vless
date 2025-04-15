dd3e4ef36d52023c1a5047f6275b3d59d217c9c3
jwfst5088 <138176547+jwfst5088@users.noreply.github.com>
2025-04-15 23:00:35 +0800

    Update vless源码.js

--- Vless_workers_pages/vless源码.js
+++ Vless_workers_pages/vless源码.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = "86c50e3a-5b87-49dd-bd20-03c7f2735e40";
+let userID = "2a05ee07-0ef3-40e2-9ef1-bdf128eaf2fe";
 
 const proxyIPs = ["ts.hpc.tw"]; 
 const cn_hostnames = [''];
